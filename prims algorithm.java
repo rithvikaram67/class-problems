12868340fc2fcb68efd5f2e446c1debd1651584d
rithvikaram67 <ramsundarrithu2006@gmail.com>
2026-04-09 15:59:58 +0530

    Create prims algorithm.java

---
+++ prims algorithm.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Main
+{
+    static final int V=3;
+    int minKey(int key[],boolean mstSet[]) {
+        int min=Integer.MAX_VALUE;
+        int minIndex=-1;
+        for(int v=0;v<V;v++) {
+            if(!mstSet[v] && key[v]<min) {
+                min=key[v];
+                minIndex=v;
+            }
+        }
+        return minIndex;
+    }
+    void printMST(int parent[],int graph[][]) {
+        System.out.println("Edge \tweight");
+        for(int i=1;i<V;i++) {
+        System.out.println(parent[i]+"-"+i+"\t"+graph[i][parent[i]]);
+        }
+    }
+    void printMST(int graph[][]) {
+        int parent[]=new int[V];
+        int key[]=new int[V];
+        boolean mstSet[]=new boolean[V];
+        for(int i=0;i<V;i++) {
+            key[i]=Integer.MAX_VALUE;
+            mstSet[i]=false;
+        }
+        key[0]=0;
+        parent[0]=-1;
+        for(int count=0;count<V-1;count++) {
+            int u=minKey(key,mstSet);
+            mstSet[u]=true;
+            for(int v=0;v<V;v++) {
+                if(graph[u][v] != 0&& !mstSet[v] && graph[u][v]<key[v]) {
+                    parent[v]=u;
+                    key[v]=graph[u][v];
+                }
+            }
+        }
+        printMST(parent,graph);
+    }
+	public static void main(String[] args) {
+	    Main t=new Main();
+	    int graph[][]={
+	        {0,4,3},
+	        {4,0,2},
+	        {3,2,0}
+	    };
+	    t.printMST(graph);
+	}
+}
